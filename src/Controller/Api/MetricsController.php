74c2f546c4c52fad1ffb889fe723146985907eda
MaratMS <maratkagyu@gmail.com>
2025-02-12 19:45:27 +0100

    Implement data collection endpoint

--- src/Controller/Api/MetricsController.php
+++ src/Controller/Api/MetricsController.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace App\Controller\Api;
 
+use App\Entity\MetricsEntry;
 use App\Service\AuthService;
 use App\Service\SourceListService;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Query\ResultSetMapping;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,7 +18,7 @@ class MetricsController extends ApiController
     public function __construct(
         private readonly SourceListService $sourceListService,
         private readonly AuthService $authService,
-        // private readonly EntityManagerInterface $entityManager,
+         private readonly EntityManagerInterface $entityManager,
     )
     {
     }
@@ -28,5 +28,38 @@ class MetricsController extends ApiController
     {
         $this->authService->requireAuthentication($request);
         return $this->json($this->sourceListService->getSourceList());
+    }
+
+    #[Route('', name: 'save_metrics', methods: ['POST'])]
+    public function saveMetrics(Request $request): Response
+    {
+        $this->authService->requireAuthentication($request);
+
+        // Getting the IP address
+        if ($request->server->get('HTTP_CF_CONNECTING_IP')) {
+            $clientIp = $request->server->get('HTTP_CF_CONNECTING_IP');
+        } elseif ($request->server->get('HTTP_X_FORWARDED_FOR')) {
+            $clientIp = $request->server->get('HTTP_X_FORWARDED_FOR');
+        } else {
+            $clientIp = $request->getClientIp();
+        }
+
+        $requestData = $request->getPayload()->all();
+
+
+        $clientName = $requestData['client'];
+        foreach ($requestData['results'] as $metricData) {
+            $metric = new MetricsEntry();
+            $metric
+                ->setClient($clientName)
+                ->setUrl($metricData['endpoint'])
+                ->setTimingMilliseconds($metricData['timingMilliseconds'])
+                ->setCreatedDateTime(new \DateTimeImmutable())
+                ->setClientIP($clientIp);
+
+            $this->entityManager->persist($metric);
+        }
+        $this->entityManager->flush();
+        return $this->json(["status" => "ok"]);
     }
 }
